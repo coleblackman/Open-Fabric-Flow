14e1131bcbfcec6851faeed190fd9565001185db
Cole Blackman <coleblax@gmail.com>
2022-07-19 09:05:15 -0400

    Init commit fabric_flow script

---
+++ fabric_flow.sh
@@ -0,0 +1,2 @@
+echo "Welcome to Open Fabric Flow. Before running this flow, ensure you have installed a technology library (PDK), OpenFPGA, and OpenLane."
+echo "You must also have already generated an FPGA fabric according to the instructions on the OpenFPGA github. That means you need to have a latest/your_design/.../SRC directory containing lb, routing, submodule, and fpga_top.v (or fpga_core.v)."
